d6fcfb962df3c6e9eccd8ff29b86e3d2f6f5b3cb
surfintime0027 <surfintime0027@hotmail.com>
2013-05-02 23:26:30 -0400

    Added some documentation to the Battle Service.

--- cgi-bin/final/GrayscalePerspective/Battle/Service.pm
+++ cgi-bin/final/GrayscalePerspective/Battle/Service.pm
@@ -1,13 +1,21 @@
 package GrayscalePerspective::Battle::Service;
 
-use GrayscalePerspective::DAL;
+# Service.pm
+# This module will facilitate all external methods for the Battle system. Things like getting collections of data as well as facilitating the actual
+# flow of Battle. Creating, actions, and ending the battle and maintaining the exp and other such ideas.
 
+use GrayscalePerspective::DAL;
 use GrayscalePerspective::Battle::Class;
 
 use base Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT = qw(getAllClasses);
 
+# getAllClasses() - Gets all the Battle Classes in the system.
+#
+# Takes no parameters.
+#
+# Returns an array reference with the list of class objects
 sub getAllClasses {
 	my @params = ();
 	my @classes_raw = @{GrayscalePerspective::DAL::execute_table_arrayref("SELECT * FROM Battle_Class")};
