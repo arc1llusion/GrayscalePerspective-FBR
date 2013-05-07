c94ab62e08abaddbf50cf88980db67167806e5ae
surfintime0027 <surfintime0027@hotmail.com>
2013-05-07 00:04:49 -0400

    Schema update for flawless execution

--- Database/Battle_Schema.sql
+++ Database/Battle_Schema.sql
@@ -9,7 +9,7 @@ DROP TABLE IF EXISTS Battle_ClassProgression;
 DROP TABLE IF EXISTS Battle_ObjectStatValue;
 DROP TABLE IF EXISTS Battle_Stat;
 DROP TABLE IF EXISTS Battle_ObjectType;
-
+DROP TABLE IF EXISTS UserCharacterMapping;
 DROP TABLE IF EXISTS Battle_Character;
 DROP TABLE IF EXISTS Battle_Class;
 
@@ -33,6 +33,23 @@ CREATE  TABLE IF NOT EXISTS `jgerma08_db`.`Battle_Character` (
     FOREIGN KEY (`ClassId` )
     REFERENCES `jgerma08_db`.`Battle_Class` (`Id` )
     ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+CREATE  TABLE IF NOT EXISTS `jgerma08_db`.`UserCharacterMapping` (
+  `UserId` INT NOT NULL ,
+  `CharacterId` INT NOT NULL ,
+  INDEX `UserKey_idx` (`UserId` ASC) ,
+  INDEX `CharacterKey_idx` (`CharacterId` ASC) ,
+  CONSTRAINT `UserKey`
+    FOREIGN KEY (`UserId` )
+    REFERENCES `jgerma08_db`.`User` (`Id` )
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `CharacterKey`
+    FOREIGN KEY (`CharacterId` )
+    REFERENCES `jgerma08_db`.`Battle_Character` (`Id` )
+    ON DELETE RESTRICT
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
